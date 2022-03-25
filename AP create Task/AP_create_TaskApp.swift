e5dbec0ef67ea3612a7fbf7974a2146968dbd133
Benjamn <bfreeman@lcs.on.ca>
2022-03-25 11:38:39 -0400

    Initial Commit

---
+++ AP create Task/AP_create_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AP_create_TaskApp.swift
+//  AP create Task
+//
+//  Created by Ben Freeman on 2022-03-25.
+//
+
+import SwiftUI
+
+@main
+struct AP_create_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
